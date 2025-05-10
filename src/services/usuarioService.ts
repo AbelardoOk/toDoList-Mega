8bb5343cf174ad3866e619cf97b56d31f30d4cdf
AbelardoOk <abelardoemailprivado@gmail.com>
2025-05-10 16:14:32 -0400

    Feat: Implementação de Validação JWT

--- src/services/usuarioService.ts
+++ src/services/usuarioService.ts
@@ -1,23 +1,42 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { pool } from "../config/db";
 import { usuario } from "../interface/usuarioInterface";
-import bcrypt, { hash } from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
 const saltRounds = 10;
 
 export const usuarioService = {
-  async register(data: usuario): Promise<usuario> {
+  async register(data: usuario): Promise<{ user: usuario; token: string }> {
     const { nome, senha, email } = data;
+
+    if (!nome || !senha || !email) {
+      throw new Error("Insira Credenciais Válidas");
+    }
 
     const hashSenha = await bcrypt.hash(senha, saltRounds);
 
     const { rows } = await pool.query(
-      `INSERT INTO usuarios (nome, senha, email) VALUES ($1, $2, $3)`,
+      `INSERT INTO usuarios (nome, senha, email) VALUES ($1, $2, $3) RETURNING *`,
       [nome, hashSenha, email]
     );
-    return rows[0];
+    const user = rows[0];
+    const token = jwt.sign(
+      { id: user.id, nome: user.nome, email: user.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: "1h" }
+    );
+
+    return { user, token };
   },
 
-  async login(data: usuario): Promise<usuario> {
+  async login(data: usuario): Promise<{ user: usuario; token: string }> {
     const { nome, senha } = data;
+
+    if (!nome || !senha) {
+      throw new Error("Insira Credenciais Válidas");
+    }
 
     const hashSenha = await pool.query(
       `SELECT senha FROM usuarios WHERE nome = $1`,
@@ -26,10 +45,26 @@ export const usuarioService = {
     const isMatch = await bcrypt.compare(senha, hashSenha.rows[0]);
     if (isMatch) {
       const { rows } = await pool.query(
-        `SELECT id FROM usuarios WHERE nome = $1`,
+        `SELECT * FROM usuarios WHERE nome = $1`,
         [nome]
       );
-      return rows[0];
+      const user = rows[0];
+
+      const token = jwt.sign(
+        { id: user.id, nome: user.nome, email: user.email },
+        process.env.JWT_SECRET!,
+        { expiresIn: "1h" }
+      );
+
+      await pool.query(
+        `
+        UPDATE usuarios
+        SET token_jwt = $1
+        WHERE id = $2`,
+        [token, user.id]
+      );
+
+      return { user, token };
     } else {
       throw new Error("Senha incorreta!");
     }
