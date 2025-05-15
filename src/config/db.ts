c7793ca7dd4d0bf2d5a56e01383c4801390d13c3
AbelardoOk <abelardoemailprivado@gmail.com>
2025-05-15 09:30:18 -0400

    Feat: Configuração para deploy

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,10 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = new Pool({
-  user: process.env.PG_USER,
-  host: "localhost",
-  database: "ToDoList",
-  password: process.env.PG_PASSWORD,
-  port: 5432,
-});
+export const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: process.env.PG_USER,
+        host: "localhost",
+        database: "ToDoList",
+        password: process.env.PG_PASSWORD,
+        port: 5432,
+      }
+);
