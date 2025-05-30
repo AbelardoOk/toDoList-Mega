3dfadeb3adedeb80cdc05519390d396deb9fd591
AbelardoOk <abelardoemailprivado@gmail.com>
2025-05-30 01:08:13 -0400

    Feat: COnfiguração Cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
+
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./docs/swagger.json";
+
 import tarefasRoute from "./routes/tarefas";
 import usuarioRoute from "./routes/usuarios";
 
@@ -9,10 +14,13 @@ const hostname = "127.0.0.1"; // Localhost
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
 
 app.use("/tarefas", tarefasRoute);
 app.use("/usuario", usuarioRoute);
 
 app.listen(port, hostname, () => {
   console.log(`Servidor iniciado em ${hostname}:${port}`);
+  console.log(`Documentação da API: ${hostname}:${port}/api-docs`);
 });
